58cfe26c082e6d94a35197691d0f654abe2cc9b8
Nico Coetzee <nicc777@gmail.com>
2022-09-25 08:42:18 +0200

    attempting to fix logout out URI

--- private-website/js/webapp.js
+++ private-website/js/webapp.js
@@ -74,7 +74,8 @@ function loginFunction() {
 
 function logoutFunction() {
     console.log("Processing LOGOUT action");
-    var logoutUrl = "https://" + cognitoAuthDomain + "/logout?client_id=" + cognitoClientId + "&logout_uri=" + applicationBaseUri + postLogoutRedirectPage + "&redirect_uri=" + applicationBaseUri + postLogoutRedirectPage;
+    // var logoutUrl = "https://" + cognitoAuthDomain + "/logout?client_id=" + cognitoClientId + "&logout_uri=" + applicationBaseUri + postLogoutRedirectPage + "&redirect_uri=" + applicationBaseUri + postLogoutRedirectPage;
+    var logoutUrl = "https://" + cognitoAuthDomain + "/logout?client_id=" + cognitoClientId + "&logout_uri=" + applicationBaseUri + postLogoutRedirectPage;
     window.location.replace(logoutUrl);
 }
 
