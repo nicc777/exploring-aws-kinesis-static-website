d0e9afceddec818059c9c1be7387bd349cd58abc
Nico Coetzee <nicc777@gmail.com>
2022-10-03 09:20:35 +0200

    Release commit for release 0.0.24-DAILY202210030920

--- gh_deployment.sh
+++ gh_deployment.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+BRANCH="${BRANCH%
+}"                # Remove a trailing newline.
+
+echo "BRANCH >>${BRANCH}<<";
+
 NOW=`date +%Y%m%d%H%M`
 VERSION=`cat VERSION`
 
 RELEASE_VERSION="${VERSION}-DAILY${NOW}"
 
 echo "Release: ${RELEASE_VERSION}"
+
+echo $RELEASE_VERSION > private-website/release_version.txt
+
+git add .
+git commit -m "Release commit for release ${RELEASE_VERSION}"
+git push origin $BRANCH
 
 gh release create r$RELEASE_VERSION --notes "Release ${RELEASE_VERSION}"
 
